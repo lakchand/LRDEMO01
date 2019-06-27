97a020cfacd2bea7a1b708494ca98393b8d42768
unknown <lakshmiks@AzureAD>
2019-06-27 14:26:47 +0530

    script0005 stored in Github and related to webtour application

---
+++ Action.c
@@ -0,0 +1,44 @@
+Action()
+{
+
+	web_url("WebTours", 
+		"URL=http://127.0.0.1:1080/WebTours/", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_think_time(8);
+
+	web_submit_data("login.pl", 
+		"Action=http://127.0.0.1:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"TargetFrame=body", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=userSession", "Value=126498.60291616zfAzHHVpiiHfDQAtApzDVDcf", ENDITEM, 
+		"Name=username", "Value=user1", ENDITEM, 
+		"Name=password", "Value=pw01", ENDITEM, 
+		"Name=JSFormSubmit", "Value=off", ENDITEM, 
+		"Name=login.x", "Value=25", ENDITEM, 
+		"Name=login.y", "Value=7", ENDITEM, 
+		LAST);
+
+	web_url("SignOff Button", 
+		"URL=http://127.0.0.1:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://127.0.0.1:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	return 0;
+}
